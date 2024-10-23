dd06719699710595a85679dde76b184267e1badd
Theo Lecuyer <tlecuyer05@gmail.com>
2024-10-22 23:50:18 -0700

    Added more command line flags

--- main.go
+++ main.go
@@ -8,23 +8,46 @@ import (
 )
 
 func main() {
+	//Flags:
+	//indexType: either inmem or runs as a db
+	//existingDB run with the name of a db to search on, no indexing will take place
+	//link will run the crawler on the link. Will output a file called index.db that can be searched on localhost
 	indexType := flag.String("index", "", "Specify the index type")
+	existingDB := flag.String("existingDB", "", "Specify an existing database to skip crawling and only run the web server")
+	link := flag.String("link", "", "Specify a link to crawl")
 	flag.Parse()
 
 	var indx Indexes
 	if *indexType == "inmem" {
 		indx = MakeInMemoryIndex()
 		go webserver(indx)
-	} else {
-		db, err := sql.Open("sqlite", "index.db")
+		if *link != "" {
+			crawl(*link, indx) //Crawl the user specified link
+		}
+	} else if *existingDB != "" {
+		// Open existing DB and run only the web server
+		db, err := sql.Open("sqlite", *existingDB)
 		if err != nil {
 			log.Printf("DB open returned %v\n", err)
+			return
 		}
 		defer db.Close()
 		indx = MakeDBIndex(db)
 		go webserver(indx)
+	} else {
+		// Open or create a new database and crawl
+		db, err := sql.Open("sqlite", "index.db")
+		if err != nil {
+			log.Printf("DB open returned %v\n", err)
+			return
+		}
+		defer db.Close()
+		indx = MakeDBIndex(db)
+		go webserver(indx)
+		if *link != "" {
+			crawl(*link, indx) //Crawl the user specified link
+		}
 	}
-	crawl("https://gihthub.com", indx)
 
 	for {
 		//Run the server until manual exit
